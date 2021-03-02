82d35037cb2e79bfda56a9e3084a93faed12a8af
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-03-02 12:49:35 +0900

    新規登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  
+  VALID_YOUTUBE_URL_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+    validates :password, presence: true, format: { with: VALID_YOUTUBE_URL_REGEX }
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+      validates :miyoji
+      validates :namae
+    end
+
+  with_options presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: '全角カタカナを使用してください' } do
+      validates :kana_miyoji
+      validates :kana_namae
+    end
 end
+
