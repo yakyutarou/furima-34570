9491fe08d78862ed866ea4d29a0e5214b97950cd
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-03-03 21:48:13 +0900

    ActiveStorage導入〜商品出品ページの表示

--- Gemfile
+++ Gemfile
@@ -60,4 +60,7 @@ gem 'devise'
 
 gem 'pry-rails'
 
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
+
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    # @item = Item.new
+  end
+
+  def create
+    # @item = Item.new(item_paramas)
+  end
+
+  # private
+
+  # def item_paramas
+  #   params.require(:item).permit(:image, :goods,:explanation,:category_id,:status_id,
+  #                                 :burden_id,:area_id,:day_id,:price).merge(user_id: current_user.id)
+  # end
 end
