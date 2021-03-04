fe99f337ecec0ceecd26fbc78ecea94115c05adf
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-03-04 17:06:26 +0900

    商品の情報をテーブルへ保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
   end
 
@@ -7,13 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_paramas)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def item_paramas
-    params.require(:item).permit(:image, :goods,:explanation,:category_id,:status_id,
-                                  :burden_id,:area_id,:day_id,:price).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, :goods,:explanation,:category_id,:status_id,:burden_id,:area_id,:day_id,:price).merge(user_id: current_user.id)
   end
 end
