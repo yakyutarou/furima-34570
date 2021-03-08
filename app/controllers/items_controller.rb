a1e37926d60e258bcc8de94bc47e6ecb3c64aec0
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-03-08 15:45:49 +0900

    rubocop自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :updete,:destroy]
-  before_action :save_params, only: [:show, :edit, :update,:destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :updete, :destroy]
+  before_action :save_params, only: [:show, :edit, :update, :destroy]
   before_action :edit_update, only: [:edit, :update]
-  
+
   def index
     @item = Item.all.order('created_at DESC')
   end
