fc4faa297f92031acad520b5681480f84587a540
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-03-11 14:20:11 +0900

    選択した商品を購入画面に表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+    @item = Item.find(params[:item_id])
+    @order_buy = OrderBuy.new
   end
+
+  def create
+  end
+  
 end
