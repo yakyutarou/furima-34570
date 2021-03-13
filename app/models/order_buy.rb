c8b2bafd6f4c792e7e3b1c52a098843351b44c3b
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-03-13 12:33:16 +0900

    単体テストコード

--- app/models/order_buy.rb
+++ app/models/order_buy.rb
@@ -5,6 +5,8 @@ class OrderBuy
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: { other_than: 0 }
+    validates :city_name,:address
+    validates :phone_number, format: {with: /\A0[0-9]{9,10}\z/}    #携帯番号・固定電話両方可、ハイフンなし
   end
 
   def save
@@ -12,5 +14,5 @@ class OrderBuy
     Order.create(postal_code: postal_code, area_id: area_id, city_name: city_name, address: address, building: building, phone_number: phone_number,buy: buy)
   end
 
-  validates :token
+  validates :token,presence: true
 end
--- spec/factories/orber_buys.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :orber_buy do
-    
-  end
-end
